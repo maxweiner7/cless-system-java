1fc95a056b64cbede6a02ef385e9d85c4218fb16
Max Weiner Ribeiro <XGFTA84@PTCOM.CORPPT.COM>
2019-06-23 19:28:21 +0100

    Placing pieces on the board

--- src/application/UI.java
+++ src/application/UI.java
@@ -11,7 +11,7 @@ public class UI {
 			}
 			System.out.println();
 		}
-		System.out.println("  a b c d e f g h");
+		System.out.print("  a b c d e f g h");
 	}
 
 	private static void printPiece(ChessPiece piece) {
@@ -19,7 +19,7 @@ public class UI {
 			System.out.print("-");
 		}
 		else {
-			System.out.println(piece);
+			System.out.print(piece);
 		}
 		System.out.print(" ");
 	}
