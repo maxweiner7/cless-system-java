fa369166eae161e55ac89559217d7b8275beb07c
Max Weiner Ribeiro <XGFTA84@PTCOM.CORPPT.COM>
2019-07-02 21:51:24 +0100

    Switching Player each turn

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,7 +17,7 @@ public class Program {
 		while (true) {
 			try {
 			UI.clearScreen();
-			UI.printBoard (chessMath.getPieces());
+			UI.printMath(chessMath); 
 			System.out.println();
 			System.out.println();
 			System.out.print("Source: ");
